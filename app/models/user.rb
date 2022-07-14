5395f3735d0da6bbeb8f65f73a7fb2a60ab62683
minhln1284 <luong.ngoc.minh@sun-asterisk.com>
2022-07-15 01:37:30 +0700

    Chapter 6 commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
                      maximum: Settings.user.email_max},
             format: {with: Settings.user.email_regex},
             uniqueness: {case_sensitive: false}
+
+  validates :password, presence: true, length: {Settings.user.password_min}
+
+  has_secure_password
+
+  before_save :downcase_email
+
+  private
+  def downcase_email
+    email.downcase!
+  end
 end
