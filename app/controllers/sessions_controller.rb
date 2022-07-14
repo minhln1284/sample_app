8a35c8dcdec2b755a80af6570474a49dfe81dcde
minhln1284 <luong.ngoc.minh@sun-asterisk.com>
2022-07-15 01:37:30 +0700

    Chapter11 commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,10 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    if @user&.authenticate params[:session][:password]
-      log_in @user
-      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
-      redirect_to root_path
+    if @user.authenticate params[:session][:password]
+      create_session
     else
-      flash.now[:danger] = t ".invalid_email_password"
+      flash.now[:danger] = t ".invalid_email_password_combination"
       render :new
     end
   end
@@ -21,11 +19,23 @@ class SessionsController < ApplicationController
   end
 
   private
+
   def find_by_email
     @user = User.find_by email: params[:session][:email].downcase
     return if @user
 
     flash[:danger] = t ".cannot_find_email"
     redirect_to login_path
+  end
+
+  def create_session
+    if @user.activated?
+      log_in @user
+      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
+      redirect_back_or @user
+    else
+      flash[:warning] = t ".message"
+      redirect_to root_url
+    end
   end
 end
