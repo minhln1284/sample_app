4b5ed73f091ea84737430cdcc85bbb4a4418e2d9
minhln1284 <luong.ngoc.minh@sun-asterisk.com>
2022-07-15 01:37:30 +0700

    Chapter 10 commit

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,18 +3,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def remember user
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
   def current_user
     if user_id = session[:user_id]
-      @current_user ||= User.find_by id: user_id if session[:user_id]
+      @current_user ||= User.find_by(id: user_id) if session[:user_id]
     elsif user_id = cookies.signed[:user_id]
       user = User.find_by id: user_id
-      if user&.authenticated? cookies[:remember_token]
+      if user&.authenticated? :remember, cookies[:remember_token]
         log_in user
         @current_user = user
       end
@@ -35,5 +29,25 @@ module SessionsHelper
     forget current_user
     session.delete :user_id
     @current_user = nil
+  end
+
+  def current_user? user
+    user == current_user
+  end
+
+  def redirect_back_or default
+    redirect_to session[:forwarding_url] || default
+    session.delete :forwarding_url
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
+  private
+  def remember user
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 end
